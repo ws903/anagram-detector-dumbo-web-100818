d14012a6f85758897e0c80b2a629e0d24f145409
ws903 <ws903@nyu.edu>
2018-10-13 14:57:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+	attr_accessor(:str)
+	def initialize(str)
+		@str = str
+	end
+
+	def match(input_match)
+		if input_match.is_a? String
+			input_match = input_match.split(' ')
+		end
+
+		input_match.select {|word|
+			word.chars.sort == @str.chars.sort
+		}
+	end
+end
